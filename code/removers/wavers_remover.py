7533373c4e41c26e73a2bc964df730d3b88eb730
Jingr33 <jirka.ingr@seznam.cz>
2025-09-06 11:10:50 +0200

    wavers bug fix

--- code/removers/wavers_remover.py
+++ code/removers/wavers_remover.py
@@ -8,16 +8,17 @@ class WaversRemover():
     def __init__(self, file_infos : list[FileInfo], backuper : Backuper) -> None:
         self._backuper = backuper
         self._file_infos = file_infos
+        self._delete_wavers()
 
-    def delete_wavers(self) -> list[FileInfo]:
+    def _delete_wavers(self) -> list[FileInfo]:
         """Remove all files with tilda beginning names."""
-        for i in tqdm(range(len(self._file_infos) - 1, -1, -1), desc="Removing ~ files"):
+        for i in range(len(self._file_infos) - 1, -1, -1):
             if '~' in self._file_infos[i].get_name():
                 self._remove_file(i)
         return self._file_infos
 
     def _remove_file(self, index : int) -> None:
         """Move file to a bin and remove it form a file_infos."""
-        file_info = self.file_infos.pop(index)
+        file_info = self._file_infos.pop(index)
         self._backuper.move_to_bin(file_info)
         ConsoleWriter.file_deleted(file_info)
